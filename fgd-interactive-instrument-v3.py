263a3cef5941933e5d952bfc06a1e233ac9eb385
breakrule <rezkymaulanagt@gmail.com>
2026-09-15 11:02:28 +0800

    chore(sidebar): rename title to 'Dashboard Geopark Kotabaru' and remove caption

--- fgd-interactive-instrument-v3.py
+++ fgd-interactive-instrument-v3.py
@@ -708,8 +708,7 @@ def render_cgk_dashboard(view_mode):
 
 # App UI
 st.sidebar.image("https://img.icons8.com/color/96/earth-element.png", width=80)
-st.sidebar.title("FGD Geopark Kotabaru")
-st.sidebar.caption("Kabupaten Kotabaru - Klaster Saijaan Bersujud")
+st.sidebar.title("Dashboard Geopark Kotabaru")
 st.sidebar.markdown("---")
 
 # Navigation option lists (two separate groups)
